d67e9951ec8fdce243cb310b764b55012b14ca04
malforsec <malforsec@gmail.com>
2024-03-28 20:17:35 +0100

    Update split_by_line.ps1

--- split_by_line.ps1
+++ split_by_line.ps1
@@ -1,5 +1,8 @@
+
+## find num of lines in file cmd: type BigDataFile.csv | find /v /c ""
+### find num of lines in file PS: (Get-Content .\BigDataFile.csv).Length
+
 # Read parent file
-## find num of lines in file: type BigDataFile.csv | find /v /c ""
 $InputFilename = Get-Content '.\file_to_split.json'
 $OutputFilenamePattern = 'output_done_'
 $LineLimit = 200000
